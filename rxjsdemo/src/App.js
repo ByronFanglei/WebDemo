2c49f0f992d95f561465c65f93d592086c8236f6
ByronFanglei <984724098@qq.com>
2021-01-10 21:49:20 +0800

    2021-1-10

--- rxjsdemo/src/App.js
+++ rxjsdemo/src/App.js
@@ -1,16 +1,26 @@
 import React, { PureComponent } from 'react'
-// import rxjs from 'rxjs'
-
-
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent, combineLatest, SubscriptionLike, PartialObserver } from 'rxjs'
+import { take, filter, map } from 'rxjs/operators'
 class App extends PureComponent{
+
   _handleButton = () => {
-    console.log('点击了button')
+    interval(500)
+    .pipe(take(10))
+    .pipe(map(x => x * 10))
+    .pipe(filter(x => x > 55))
+    .subscribe(console.log)
+  }
+
+  _cancleButton = () => {
+    alert('取消订阅关系')
+    // 取消订阅关系
+    this.sub1$.unsubscribe()
+    this.sub2$.unsubscribe()
+    console.log(this.subject)
   }
 
   componentDidMount() {
-    var button = document.querySelector('button')
-    // button.addEventListener('click', this._handleButton)
-    // rxjs.interval(500).pipe(rxjs.operators.take(4)).subscribe(console.log)
+    // interval(500).pipe(take(5)).subscribe(console.log)
   }
 
   componentWillUnmount() { 
@@ -21,7 +31,8 @@ class App extends PureComponent{
   render() {
     return(
       <div>
-        <button>Hello</button>
+        <button onClick={this._handleButton}>Hello</button>
+        <button onClick={this._cancleButton}>cancle</button>
       </div>
     )
   }
